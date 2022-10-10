f9ad96ea3635777e98b860e642823c005c7d4eff
mav006 <59050192+mav006@users.noreply.github.com>
2022-10-10 10:26:03 +0200

    Update Host.cs

--- Assets/Scripts/Host.cs
+++ Assets/Scripts/Host.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class Host : MonoBehaviour
 {
+    Socket newSocket;
     // Start is called before the first frame update
     void Start()
     {
-        
+        newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(“192.168.204.31”), port);
+        //newSocket.Bind(ipep);
     }
 
     // Update is called once per frame
@@ -15,4 +20,7 @@ public class Host : MonoBehaviour
     {
         
     }
+
+    // https://forum.unity.com/threads/tcp-sockets-how-to-receive-from-client.541502/
+
 }
