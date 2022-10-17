8a952a273446dc1be1931c384c16ab7f3166b038
mav006 <59050192+mav006@users.noreply.github.com>
2022-10-17 10:32:25 +0200

    Connection made!

--- Assets/Scripts/ClientUDP.cs
+++ Assets/Scripts/ClientUDP.cs
@@ -1,18 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System;
+using System.Net;
+using System.Text;
 using UnityEngine;
+using System.Threading;
 
 public class ClientUDP : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    int recv;
+    byte[] data;
+    IPEndPoint ipep;
+    Socket newsock;
+    IPEndPoint server;
+    EndPoint Remote;
+    string message;
+
+    Thread myThread;
+
+    private void clientConnection()
     {
-        
+        Debug.LogWarning("Starting Thread");
+
+        Debug.Log("Sending Message");
+
+        server = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+        Remote = (EndPoint)(server);
+
+        message = "Hi i want to connect";
+        data = Encoding.ASCII.GetBytes(message);
+        newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+
+        //recv = newsock.ReceiveFrom(data, ref Remote);
+        //Debug.Log(Remote.ToString());
+
+
+        Debug.Log(Encoding.ASCII.GetString(data, 0, recv));
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        
+        data = new byte[1024];
+        ipep = new IPEndPoint(IPAddress.Any, 9050);
+
+        newsock = new Socket(AddressFamily.InterNetwork,
+                        SocketType.Dgram, ProtocolType.Udp);
+
+        myThread = new Thread(clientConnection);
+        myThread.Start();
+
     }
+
+    void onDisable()
+    {
+        try
+        {
+            myThread.Abort();
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+
 }
+
