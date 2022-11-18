cf6058dabcfdced99c41d963b4fd0ab74a8ba356
Paula Hitz <paulahitz8@gmail.com>
2022-11-18 12:21:12 +0100

    ApplyColorToUI

---
+++ Assets/Scripts/ApplyMaterial.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyMaterial : MonoBehaviour
+{
+    [SerializeField] private int colorID = 0;
+    [SerializeField] private Material material;
+    [SerializeField] private GameObject player;
+
+    public void ChangeColor()
+    {
+        player.GetComponent<MeshRenderer>().material = material;
+    }
+}
