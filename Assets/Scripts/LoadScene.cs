0a91c84ebb188b7ec5b29cd2c36c6a5f87181320
Paula Hitz <paulahitz8@gmail.com>
2022-12-12 09:57:37 +0100

    Update LoadScene.cs

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class LoadScene
+public class LoadScene : MonoBehaviour
 {
     public void LoadNextScene(string m_NextScene)
     {
