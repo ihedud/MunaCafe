d88f7d61718f95ae3e14053e3b79f48540c4a667
ihedud <59050152+ihedud@users.noreply.github.com>
2023-01-22 11:19:47 +0100

    better now

--- Assets/Scripts/Gameplay/Machines/DonutStation.cs
+++ Assets/Scripts/Gameplay/Machines/DonutStation.cs
@@ -162,7 +162,7 @@ public class DonutStation : MonoBehaviour
 
         timer += Time.deltaTime;
         if(currentState != State.Finished)
-            sphere.transform.localScale += new Vector3(0.000009f, 0.000025f, 0.000009f);
+            sphere.transform.localScale += new Vector3(0.0002f, 0.0002f, 0.0002f);
 
         if (timer > updateTime)
         {
